a7bf2d0fee280223c6a22719e3e73cac7ac798db
Norah <sq11y@proton.me>
2025-12-13 14:46:12 +0100

    Remove hardcoded value

--- packages/docs/src/scaffold.ts
+++ packages/docs/src/scaffold.ts
@@ -2,6 +2,6 @@ import { cp } from 'fs/promises';
 import { join } from 'path';
 import { searchForWorkspaceRoot } from 'vite';
 
-const target = join(searchForWorkspaceRoot(process.cwd()), 'example', 'docs');
+const target = join(searchForWorkspaceRoot(process.cwd()), 'docs');
 
 await cp(join(import.meta.dirname, '../template'), target, { recursive: true });
